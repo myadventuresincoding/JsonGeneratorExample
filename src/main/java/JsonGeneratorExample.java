751baa0a3e7ca6e24963010a66c466acefd7741c
myadventuresincoding <66219262+myadventuresincoding@users.noreply.github.com>
2020-07-11 18:02:34 -0600

    Add additional comments explaining the example

--- src/main/java/JsonGeneratorExample.java
+++ src/main/java/JsonGeneratorExample.java
@@ -7,15 +7,16 @@ import java.io.ByteArrayOutputStream;
 public class JsonGeneratorExample {
     public static void main(String[] args) throws Exception {
 
+        // Create the OutputStream we will be writing JSON to
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
+        // Create a background thread to print out the contents of the OutputStream
         PrintOutputStreamRunnable runnable = new PrintOutputStreamRunnable(outputStream);
-
         Thread thread = new Thread(runnable);
         thread.start();
 
+        // Create the JsonGenerator to write to our OutputStream
         JsonFactory factory = new JsonFactory();
-
         JsonGenerator generator = factory.createGenerator(outputStream, JsonEncoding.UTF8);
 
         generator.writeStartObject();
